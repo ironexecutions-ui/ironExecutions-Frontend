34fd9e96ae71561927349c34a55f0fc9c8bb01e7
Andy de Oliveira <elderromero565@gmail.com>
2025-11-13 13:37:38 -0300

    Enviando todo o projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+import Header from "./components/header";
+import Rodape from "./components/rodape";
+
+export default function App() {
+  return (
+    <>
+      <h1>ola pessoal</h1>
+      <Rodape />
+      <Header />
+    </>
+  )
+}
---
+++ src/components/rodape.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './rodape.css'
+
+export default function Rodape() {
+    return (
+        <h2>rodape</h2>
+
+    )
+}
